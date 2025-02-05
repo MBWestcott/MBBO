40f37546916a72fb3ec09d02f3fa8aeca91b244c
Mike Westcott <mike.westcott@faulconbridge.co.uk>
2025-02-05 16:02:21 +0000

    To do week 4: (1) logarithmic scaling on the contamination function (2) Function 5 - optmize for unimodal (3) Test with functions with more (esp. in higher dimensions) and narrower peaks; trying EI acquisition functions, and/or decreasing kappa
    Week 4:
    (1) (from (3) above) - enhance two_d_test for n dimensions - done
    (2) (from (3) above) - further enhance for n peaks - done
    (3) function 5 - optimize for unimodal - set up test profiles.
    Finding that the calibration process runs slowly for higher dimensions. To a degree, can mitigate this with coarser grid (20 or 30 points per axis instead of 100)
    Used _kernel.length_scale to pick up length scales trained from calibration functions and real data.
    Repeated calibrations using only that length scale for each function (functions 2 and 8 didn't find optimal lengthscale in training - will need to revisit)
    Used that to do CSV output over 4 random test objective functions for each competition function. Determined best kappa based on looking at CSV outputs (but only got one for first 3 functions)
    Reran the "suggested next" still using RBF, with trained lengthscale, and best kappa as far as can be worked out:

--- notebooks/output.py
+++ notebooks/output.py
@@ -1,5 +1,6 @@
 def format_for_submission(number_array):
     lst = number_array.tolist()
     formatted_numbers = [f"{n:.6f}" for n in lst]
+    formatted_numbers = str.replace(formatted_numbers, "1.000000", "0.999999")
     result = "-".join(formatted_numbers)
     return result
